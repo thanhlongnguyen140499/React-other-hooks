7b0269f7bcd837655f475f686ce22120824cb3a4
Long (Edward) T. NGUYEN <edward@enclave.vn>
2023-05-15 15:28:26 +0700

    dev: update - useTransition

--- src/App.js
+++ src/App.js
@@ -1,66 +1,44 @@
 import logo from "./logo.svg";
 import "./App.css";
-import { useCallback, useMemo, useReducer, useState } from "react";
+import {
+  useCallback,
+  useMemo,
+  useReducer,
+  useState,
+  useTransition,
+} from "react";
 import List from "./List";
-import Todo from "./Todo";
-
-export const ACTIONS = {
-  INCREMENT: "increment",
-  DECREMENT: "decrement",
-  ADD_TODO: "add-todo",
-  TOGGLE_TODO: "toggle-todo",
-  DELETE_TODO: "delete-todo",
-};
-
-const reducer = (todos, action) => {
-  switch (action.type) {
-    case ACTIONS.ADD_TODO:
-      return [...todos, newTodo(action.payload.name)];
-    case ACTIONS.TOGGLE_TODO:
-      return todos.map((todo) => {
-        if (todo.id === action.payload.id) {
-          return { ...todo, complete: !todo.complete };
-        }
-        return todo;
-      });
-    case ACTIONS.DELETE_TODO:
-      return todos.filter((todo) => todo.id !== action.payload.id);
-    default:
-      return todos;
-  }
-};
-
-const newTodo = (name) => {
-  return { id: Date.now(), name: name, complete: false };
-};
+// import Todo from "./Todo";
 
 function App() {
-  const [todos, dispatch] = useReducer(reducer, []);
-  const [name, setName] = useState("");
+  const [isPending, startTransition] = useTransition();
+  const [input, setInput] = useState("");
+  const [list, setList] = useState([]);
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    dispatch({
-      type: ACTIONS.ADD_TODO,
-      payload: {
-        name: name,
-      },
+  const LIST_SIZE = 20000;
+
+  const handleChange = (e) => {
+    setInput(e.target.value);
+    startTransition(() => {
+      const l = [];
+      for (let i = 0; i < LIST_SIZE; i++) {
+        l.push(e.target.value);
+      }
+
+      setList(l);
     });
-    setName("");
   };
 
   return (
     <>
-      <form onSubmit={handleSubmit}>
-        <input
-          type="text"
-          value={name}
-          onChange={(e) => setName(e.target.value)}
-        />
-      </form>
-      {todos.map((todo) => {
-        return <Todo key={todo.id} todo={todo} dispatch={dispatch} />;
-      })}
+      <input type="text" value={input} onChange={handleChange} />
+      <ul>
+        {isPending
+          ? "Loading..."
+          : list.map((item, index) => {
+              return <li key={index}>{item}</li>;
+            })}
+      </ul>
     </>
   );
 }
